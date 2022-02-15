b8f2c5114332537e1cbeb397cb8abda68c644173
GuiNo12 <guilherme.nogueiracoelho@live.com>
2022-02-15 11:07:31 -0300

    Autocompletar endereço ao digitar CEP
    Autocompletar endereço ao digitar CEP no cadastro de funcinários.

--- src/pages/CadastrarFuncionario/index.jsx
+++ src/pages/CadastrarFuncionario/index.jsx
@@ -1,6 +1,6 @@
 import Form from 'react-bootstrap/Form';
 import './func.css';
-import { RequisitaCadastrarFuncionario, RequisitaCadastrarMedico } from "../../api/api";
+import { RequisitaCadastrarFuncionario, RequisitaCadastrarMedico, RequisitaEndereco } from "../../api/api";
 import { useState, useEffect } from 'react';
 
 export function CadastrarFuncionario() {
@@ -24,16 +24,31 @@ export function CadastrarFuncionario() {
   const handleOnChange = () => {
     setIsChecked(!isChecked);
     const handleSubmit = async () => {
-      let response = await RequisitaCadastrarMedico(nome,email,telefone,cep,logradouro,bairro,cidade,estado,dataContrato,salario,senhaHash, especialidade, CRM)
-      console.log("Response handlesubmit: ",response)
+      let response = await RequisitaCadastrarMedico(nome, email, telefone, cep, logradouro, bairro, cidade, estado, dataContrato, salario, senhaHash, especialidade, CRM)
+      console.log("Response handlesubmit: ", response)
       alert(response)
     }
   };
 
+  const buscarEndereco = async (cep) => {
+    let response = '';
+    let tam = cep + "";
+    setCep(cep);
+
+    if (tam.length == 8) {
+      response = await RequisitaEndereco(cep);
+
+      setLogradouro(response.logradouro);
+      setBairro(response.bairro);
+      setCidade(response.cidade);
+      setEstado(response.estado);
+    }
+  }
+
 
   const handleSubmit = async () => {
-    let response = await RequisitaCadastrarFuncionario(nome,email,telefone,cep,logradouro,bairro,cidade,estado,dataContrato,salario,senhaHash, especialidade, CRM)
-    console.log("Response handlesubmit: ",response)
+    let response = await RequisitaCadastrarFuncionario(nome, email, telefone, cep, logradouro, bairro, cidade, estado, dataContrato, salario, senhaHash, especialidade, CRM)
+    console.log("Response handlesubmit: ", response)
     alert(response)
   }
 
@@ -48,7 +63,7 @@ export function CadastrarFuncionario() {
           <div class="col-lg-12">
             <Form.Group>
               <Form.Label>Nome Completo</Form.Label>
-              <Form.Control type="text" class="form-control" name="nome" placeholder="" value={nome} onChange={e => setNome(e.target.value)} required/>
+              <Form.Control type="text" class="form-control" name="nome" placeholder="" value={nome} onChange={e => setNome(e.target.value)} required />
             </Form.Group>
           </div>
         </div>
@@ -57,70 +72,70 @@ export function CadastrarFuncionario() {
           <div class="col-lg-8">
             <Form.Group>
               <Form.Label>Email</Form.Label>
-              <Form.Control type="text" class="form-control" name="email" placeholder="" value={email} onChange={e => setEmail(e.target.value)} required/>
+              <Form.Control type="text" class="form-control" name="email" placeholder="" value={email} onChange={e => setEmail(e.target.value)} required />
             </Form.Group>
           </div>
           <div class="col-lg-4">
             <Form.Group>
               <Form.Label>Telefone</Form.Label>
-              <Form.Control type="text" class="form-control" name="telefone" placeholder="Apenas números" value={telefone} onChange={e => setTelefone(e.target.value)} required/>
+              <Form.Control type="text" class="form-control" name="telefone" placeholder="Apenas números" value={telefone} onChange={e => setTelefone(e.target.value)} required />
             </Form.Group>
           </div>
         </div>
         <Form.Label></Form.Label>
         <div class="row">
-        <div class="col-lg-6">
-                <Form.Group>
-                  <Form.Label>Logradouro</Form.Label>
-                  <Form.Control type="logradouro" placeholder="" value={logradouro} onChange={e => setLogradouro(e.target.value)} required/>
-                </Form.Group>
-              </div>
-              <div class="col-lg-6">
-                <Form.Group>
-                  <Form.Label>Bairro</Form.Label>
-                  <Form.Control type="bairro" placeholder="" value={bairro} onChange={e => setBairro(e.target.value)} required/>
-                </Form.Group>
-              </div>
+          <div class="col-lg-6">
+            <Form.Group>
+              <Form.Label>Logradouro</Form.Label>
+              <Form.Control type="logradouro" placeholder="" value={logradouro} onChange={e => setLogradouro(e.target.value)} required />
+            </Form.Group>
           </div>
-          <Form.Label></Form.Label>
-          <div class="row">
-              <div class="col-lg-6">
-                <Form.Group>
-                  <Form.Label>Cidade</Form.Label>
-                  <Form.Control type="cidade" placeholder="" value={cidade} onChange={e => setCidade(e.target.value)} required/>
-                </Form.Group>
-              </div>
-              <div class="col-lg-3">
-                <Form.Group>
-                  <Form.Label>Estado</Form.Label>
-                  <Form.Control type="estado" placeholder="Ex.: Minas Gerais" value={estado} onChange={e => setEstado(e.target.value)} required/>
-                </Form.Group>
-              </div>
-              <div class="col-lg-3">
-                <Form.Group>
-                  <Form.Label>CEP</Form.Label>
-                  <Form.Control type="cep" placeholder="Apenas números" value={cep} onChange={e => setCep(e.target.value)} required/>
-                </Form.Group>
-              </div>
+          <div class="col-lg-6">
+            <Form.Group>
+              <Form.Label>Bairro</Form.Label>
+              <Form.Control type="bairro" placeholder="" value={bairro} onChange={e => setBairro(e.target.value)} required />
+            </Form.Group>
           </div>
+        </div>
+        <Form.Label></Form.Label>
+        <div class="row">
+          <div class="col-lg-6">
+            <Form.Group>
+              <Form.Label>Cidade</Form.Label>
+              <Form.Control type="cidade" placeholder="" value={cidade} onChange={e => setCidade(e.target.value)} required />
+            </Form.Group>
+          </div>
+          <div class="col-lg-3">
+            <Form.Group>
+              <Form.Label>Estado</Form.Label>
+              <Form.Control type="estado" placeholder="Ex.: Minas Gerais" value={estado} onChange={e => setEstado(e.target.value)} required />
+            </Form.Group>
+          </div>
+          <div class="col-lg-3">
+            <Form.Group>
+              <Form.Label>CEP (Digite para autocompletar endereço)</Form.Label>
+              <Form.Control type="cep" placeholder="Apenas números" maxLength={8} value={cep} onChange={e => buscarEndereco(e.target.value)} required />
+            </Form.Group>
+          </div>
+        </div>
         <Form.Label></Form.Label>
         <div class="row">
           <div class="col-lg-4">
             <Form.Group>
               <Form.Label>Data de Início do Contrato de Trabalho</Form.Label>
-                <input type="text" class="form-control" name="dataContrato" placeholder="dia/mês/ano" value={dataContrato} onChange={e => setDataContrato(e.target.value)} required/>
+              <input type="text" class="form-control" name="dataContrato" placeholder="dia/mês/ano" value={dataContrato} onChange={e => setDataContrato(e.target.value)} required />
             </Form.Group>
           </div>
           <div class="col-lg-4">
             <Form.Group>
               <Form.Label>Salário (R$)</Form.Label>
-              <Form.Control type="text" class="form-control" name="salario" placeholder="Ex: 2200.00" value={salario} onChange={e => setSalario(e.target.value)} required/>
+              <Form.Control type="text" class="form-control" name="salario" placeholder="Ex: 2200.00" value={salario} onChange={e => setSalario(e.target.value)} required />
             </Form.Group>
           </div>
           <div class="col-lg-4">
             <Form.Group>
               <Form.Label>Senha</Form.Label>
-              <Form.Control type="text" class="form-control" name="senhaHash" placeholder="Até 30 caracteres" value={senhaHash} onChange={e => setSenhaHash(e.target.value)} required/>
+              <Form.Control type="text" class="form-control" name="senhaHash" placeholder="Até 30 caracteres" value={senhaHash} onChange={e => setSenhaHash(e.target.value)} required />
             </Form.Group>
           </div>
         </div>
@@ -130,9 +145,9 @@ export function CadastrarFuncionario() {
             <Form.Group class="alert alert-warning" >
               <Form.Label >Marque o checkbox e Preencha os campos abaixo somente se for médico</Form.Label>
               <div className="topping">
-                <input type="checkbox"  id="topping" name="topping" value="CadastroFuncMedico" checked={isChecked} onChange={handleOnChange} />
-                     Sou Médico(a)
-                </div>
+                <input type="checkbox" id="topping" name="topping" value="CadastroFuncMedico" checked={isChecked} onChange={handleOnChange} />
+                Sou Médico(a)
+              </div>
             </Form.Group>
           </div>
         </div>
@@ -141,13 +156,13 @@ export function CadastrarFuncionario() {
           <div class="col-lg-8">
             <Form.Group class="alert alert-warning" >
               <Form.Label>Especialidade</Form.Label>
-              <Form.Control type="text" class="form-control" name="especialidade" placeholder="Ex.: Pediatria" value={especialidade} onChange={e => setEspecialidade(e.target.value)} required/>
+              <Form.Control type="text" class="form-control" name="especialidade" placeholder="Ex.: Pediatria" value={especialidade} onChange={e => setEspecialidade(e.target.value)} required />
             </Form.Group>
           </div>
           <div class="col-lg-4">
             <Form.Group class="alert alert-warning" >
               <Form.Label>CRM</Form.Label>
-              <Form.Control type="text" class="form-control" name="CRM" placeholder="000000" value={CRM} onChange={e => setCrm(e.target.value)} required/>
+              <Form.Control type="text" class="form-control" name="CRM" placeholder="000000" value={CRM} onChange={e => setCrm(e.target.value)} required />
             </Form.Group>
           </div>
         </div>
